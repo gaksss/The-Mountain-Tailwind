3955aea52df569cc3cfe09bc19fc05f450ba262d
gaksss <clement.carlier2@gmail.com>
2024-11-04 11:54:18 +0100

    navbar et menu burger fini

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    './index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mountain-bg' : "url('images/bg_header.jpg')",
+      },
+      colors: {
+        primary: {
+          orange: 'hsl(35, 77%, 62%)',
+          red: 'hsl(5, 85%, 63%)',
+        },
+        neutral: {
+          'off-white': 'hsl(36, 100%, 99%)',
+          'grayish-blue': 'hsl(233, 8%, 79%)',
+          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+          'very-dark-blue': 'hsl(240, 100%, 5%)',
+        },
+      },
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        paragraph: '20px',
+      },
+    },
   },
   plugins: [],
 }
-
